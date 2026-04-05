121c70d5119f7e58ae065399e3abfc2baa1d6e9c
HSK-DunDunRST <gon5073@naver.com>
2026-04-06 00:41:08 +0900

    CREATE - Initial Project Created

---
+++ src/main/java/com/hskgroup/profile/LocalBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.hskgroup.profile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hskgroup/profile/LocalBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hskgroup.profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
